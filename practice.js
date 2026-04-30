6a6621989267f28bf328e9a9f83c93e1347c38b7
Anshu Kharwar <anshu.builds@gmail.com>
2026-04-30 16:44:38 +0530

    Add card styling and enhance form functionality for note creation

--- practice.js
+++ practice.js
@@ -1,16 +1,90 @@
 
+
 let ui = document.querySelector(".notes");
 let add = document.querySelector(".add");
 let form = document.querySelector("form");
 let close = document.querySelector(".close");
 
-
-add.addEventListener("click", function() {
+function hideNote() {
   form.style.display = "flex";
   ui.style.display = "none";
-});
+}
 
-close.addEventListener("click", function() {
+function hideForm() {
   form.style.display = "none";
   ui.style.display = "flex";
+}
+
+
+add.addEventListener("click", hideNote);
+
+close.addEventListener("click", hideForm);
+
+/*<div class="card">
+        <h1>Name</h1>
+        <p>Hometown : <span class="hometown">Hometown</span></p>
+        <p>Purpose : <span>Purpose</span></p>
+        <div>
+          <button class="call">Call</button>
+          <button class="msg">Message</button>
+        </div>
+      </div>
+      */
+
+form.addEventListener("submit", function(data){
+  data.preventDefault();
+  naam = data.target[0].value;
+  homeTown = data.target[1].value;
+  purpose = data.target[2].value;
+  phone = data.target[3].value;
+  email = data.target[4].value;
+  category = data.target[5].value;
+
+  let div = document.createElement("div");
+  div.setAttribute("class", "card");
+  document.body.querySelector(".main").prepend(div);
+
+  let h1 = document.createElement("h1");
+  h1.innerText = naam;
+  document.body.querySelector(".card").append(h1);
+
+
+  let p1 = document.createElement("p");
+  p1.setAttribute("class", "p1");
+  p1.innerText = "Hometown : "
+  document.body.querySelector(".card").append(p1);
+  let span1 = document.createElement("span");
+  span1.innerText = homeTown;
+  document.body.querySelector(".card").querySelector(".p1").append(span1);
+  
+
+
+  let p2 = document.createElement("p");
+  p2.setAttribute("class", "p2");
+  p2.innerText = "Purpose : "
+  document.body.querySelector(".card").append(p2);
+  let span2 = document.createElement("span");
+  span2.innerText = purpose;
+  document.body.querySelector(".card").querySelector(".p2").append(span2);
+  
+  
+  let div1= document.createElement("div");
+  div1.setAttribute("class", "div1");
+  document.body.querySelector(".card").append(div1);
+
+  let btn1 = document.createElement("button");
+  btn1.setAttribute("class", "call");
+  btn1.innerText = "Call";
+  document.body.querySelector(".card").querySelector(".div1").append(btn1);
+
+  let btn2 =document.createElement("button");
+  btn2.setAttribute("class", "msg");
+  btn2.innerText = "Message";
+  document.body.querySelector(".card").querySelector(".div1").append(btn2);
+
+  hideForm();
+
+  
+
+  console.log(naam, homeTown, purpose, phone, email, category);
 })
